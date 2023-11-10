db974bd7ea50c90a461fb35470dff5322a9780b7
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2023-11-10 21:10:18 +0530

    js

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+// 1. word vs keyword
+//2. var const let
+//3. variable and constants
+//4. hoisting- variable baane se pehle usse ham use kar sakte hai
+// console.log(a);
+// var a=4;
+//5. Types in js- primitive & reference
+// primitive
+// reference= [](){}
+// aisi koi bhi value jisko copy karne par real copy nahi hota, balki us main value ka reference pass hojata
+// hai, use hum reference value kahte hai, aur jiska real karne par real copy hojae wo value primitive type 
+// hoti hai.
+// var a=[1,2,3,4]
+// var b=a;
+// b.pop()//this will also remove last element from a too!! this is reference
+//6. Conditionals- if else else-if 
+//7. loops
+//8. functions
+//9. arrays
+//10. push, pop, shift, unshift, slice
+//11. DOM- objects
+///-blank objects
+    var a={};
+///-filled objects
+    var a={
+        age: 24,
+        name: "Harsh",
+        email: "email@tcom"
+    }
