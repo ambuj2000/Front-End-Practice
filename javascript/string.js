9366d3c98962688e0ed075d7a4d44ce7fec00535
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2023-12-24 22:05:07 +0530

    Create strings

---
+++ javascript/string.js
@@ -0,0 +1,24 @@
+//strings are immutable in JS
+
+str="Ambuj";
+newstr=str.toUpperCase();
+console.log(newstr);
+
+newstr1=str.toLowerCase();
+console.log(newstr1);
+
+
+str2="    hello ji kaise ho aap      ";
+console.log(str2.trim());//remove white spaces before and after the string
+
+str1="012383832";
+console.log(str1.slice(2,5));//238
+
+let res=str1.concat(str2);
+console.log(res);//joins the string
+
+let str3="ILoveJS";
+console.log(str3.charAt(2));
+
+str3=str3.replace("I","S");// to repalce I with S
+console.log(str3);
