722ef84d73921f0f0578a3f83c7d0fb6c534add7
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2023-12-29 10:13:06 +0530

    Create function.js

---
+++ javascript/function.js
@@ -0,0 +1,24 @@
+//FUNCTION IN JS
+
+//sum
+function sum(a,b){
+    return a+b;
+}
+
+//multiply
+
+function multiply(a,b){
+    return a*b;
+}
+
+let res=multiply(4,38);
+
+
+//Arrow function
+const arrowSum=(a,b)=>{
+        console.log(a+b);
+};
+
+const arrowMult=(a,b)=>{
+    return a*b;
+};
