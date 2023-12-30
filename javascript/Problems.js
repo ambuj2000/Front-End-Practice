d37dbca669ac1a26900b15d252c4c6c55e024c5c
Ambuj Singh <57827514+ambuj2000@users.noreply.github.com>
2023-12-30 10:33:58 +0530

    Update Problems.js

--- javascript/Problems.js
+++ javascript/Problems.js
@@ -131,5 +131,21 @@ const ans= (str) => {
    return cnt;
 }
 
+// Q.8 For a given array of numbers, print the square of each value using the forEach loop
+
+let arr=[1,2,3,4,5];
+
+arr.forEach((val)=>{
+    console.log(val*val);//val**2
+});
+
+//another way of callback function
+
+let calcusquare= (val) => {
+    console.log(val*val);//val**2
+};
+
+arr.forEach(calcusquare);
+
 
 
